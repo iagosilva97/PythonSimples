69fba039c7a746bf7902ebc8a1b160ec4090239c
iagosilva97 <iagosilva.mateus@outlook.com>
2025-09-16 18:17:59 -0300

    envio aulas parti2

--- PythonDev/aula8.py
+++ PythonDev/aula8.py
@@ -7,5 +7,10 @@ muito consagrado na industria
 print(movieName.upper())#maiusculo
 print(movieName.lower())#minusculo
 print(movieName.capitalize())#primeira maiuscula
-print(movie)
-
+print(movieName.title())#todas as primeiras maiusculas
+print(movieName.strip())#remove espaços desnecessarios
+print(movieName.center(10, "-"))#retorna a string centralizada com caracteres adicionais
+print(movieName.replace("o", "0"))#substitui caracteres
+print(movieName.split(" "))#divide a string em uma lista
+print(movieName.find("Gun"))#retorna o indice do inicio da palavra
+print(movieName.count("o"))#conta quantas vezes o caractere aparece
